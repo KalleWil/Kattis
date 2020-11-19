742245a87f65d03d8d068f2dff85ecc9c813430b
KalleWil <kalle-wil@hotmail.com>
2020-11-19 12:40:43 +0100

    Test

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        System.out.println(n);
+        System.out.println(n + 5);
 
     }
 }
